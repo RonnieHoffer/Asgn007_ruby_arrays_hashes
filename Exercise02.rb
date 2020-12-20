fb77467bfbabc23221a982b0331cc26e2052f7d4
RonnieHoffer <ronnie.hoffer@gmail.com>
2020-12-20 14:09:26 -0500

    Adding exercise 2, complete

--- Exercise02.rb
+++ Exercise02.rb
@@ -1,1 +1,30 @@
-grocery list = ["salmon", "almonds", "green beans", "squash"]
+def print_list(list)
+    list.delete("salmon")
+    length = list.length
+    puts "Number of items on list = #{length}"
+    puts "The second itme on the list (before sorting) is #{list[1]}"
+    counter = 0
+    bananas = false
+    
+    while counter < length
+        puts "* #{list[counter]}"
+        if list[counter] == "bananas"
+            bananas = true
+        end
+        counter += 1
+    end
+
+    if bananas == false
+        puts "You do not need to pick up bananas today"
+    elsif bananas == true
+        puts "You need to pick up bananas"
+    end
+end
+
+groceries = ["salmon", "almonds", "green beans", "squash"]
+print_list(groceries.sort)
+puts "\n"
+
+groceries.push("rice")
+print_list(groceries.sort)
+puts "\n"
