88ace6e99b48b989fb12151720661770304bd9c0
RonnieHoffer <ronnie.hoffer@gmail.com>
2020-12-27 20:35:14 -0500

    This Assignment is now complete

--- Exercise05.rb
+++ Exercise05.rb
@@ -1,23 +1,36 @@
 def chase_squirrel(dogs)
-    dogs.map! {|dog| dog[:name] && dog[:position] + 5}
-    puts "#{dogs}"
+    dogs.map! do |dog|
+        dog[:position] += 5
+        dog
+    end
     return dogs
 end
 
+
+
 def get_absent_dogs(dogs)
-    dogs.select! {|dog| dog[:position] > 10}
-    absent_list = []
-    dogs.each do |dog|
-        absent_list.push(dog[:name])
-    end
-    return absent_list
+    return absent_list = dogs.select {|dog| dog[:position] > 10}
 end
+
+
 
 def call_absent_dogs(absent_dogs)
     absent_dogs.each do |absent_dog|
-        puts "Come back #{absent_dog}!"
+        puts "Come back #{absent_dog[:name]}!"
     end
 end
+
+
+
+def round_up_dogs(dogs)
+    dogs.map! do |dog|
+        dog[:position] = 0
+        dog
+    end
+    return dogs
+end
+
+
 
 my_dogs = [
   { :name => "Ralph", :position => 7 },
@@ -25,18 +38,49 @@ my_dogs = [
   { :name => "Jade", :position => 11 },
 ]
 
+
+absent_dogs = get_absent_dogs(chase_squirrel(my_dogs))
+puts "The following dogs are absent from my dogs: #{absent_dogs}"
+call_absent_dogs(absent_dogs)
+puts "\n"
+home_dogs = round_up_dogs(my_dogs)
+puts "The dogs have come home."
+puts home_dogs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # her_dogs = [
 #   { :name => 'Bentley', :position => 12 },
 #   { :name => 'Bogie', :position => 8 },
 #   { :name => 'Bernie', :position => 4 },
 # ]
 
-chase_squirrel(my_dogs)
-
-# absent_dogs = get_absent_dogs(chase_squirrel(my_dogs))
-# puts "The following dogs are absent from my dogs: #{absent_dogs}"
-# call_absent_dogs(absent_dogs)
-# puts "\n"
 
 # absent_dogs = get_absent_dogs(her_dogs)
 # puts "The following dogs are absent from her dogs: #{absent_dogs}"
